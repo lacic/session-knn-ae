c610a4098024e0b47b75e8a6a64f5e82873d1c96
samuelru <samy.rgg@gmail.com>
2020-01-22 16:03:22 +0100

    Adds folders that are missing.

--- generate_folders.py
+++ generate_folders.py
@@ -5,17 +5,18 @@ import sys
 def generate_folder_structure(dataset):
     print("Generating Folders for: " + dataset)
     root = "data"
+    os.makedirs("plots/results")
     os.makedirs(root + "/" + dataset + "/raw")
     os.makedirs(root + "/" + dataset + "/interim/infer")
     os.makedirs(root + "/" + dataset + "/interim/models")
     os.makedirs(root + "/" + dataset + "/interim/predict/base")
     os.makedirs(root + "/" + dataset + "/interim/predict/hyperparam")
-    os.makedirs(root + "/" + dataset + "/interim/processed/eval/all")
-    os.makedirs(root + "/" + dataset + "/interim/processed/eval/next")
-    os.makedirs(root + "/" + dataset + "/interim/processed/eval/base/all")
-    os.makedirs(root + "/" + dataset + "/interim/processed/eval/base/next")
-    os.makedirs(root + "/" + dataset + "/interim/processed/eval/hyperparam/all")
-    os.makedirs(root + "/" + dataset + "/interim/processed/eval/hyperparam/next")
+    os.makedirs(root + "/" + dataset + "/processed/eval/all")
+    os.makedirs(root + "/" + dataset + "/processed/eval/next")
+    os.makedirs(root + "/" + dataset + "/processed/eval/base/all")
+    os.makedirs(root + "/" + dataset + "/processed/eval/base/next")
+    os.makedirs(root + "/" + dataset + "/processed/eval/hyperparam/all")
+    os.makedirs(root + "/" + dataset + "/processed/eval/hyperparam/next")
 
 
 if __name__ == "__main__":
